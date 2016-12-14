5ff7a9339126dc2061ab62be5d76db500a2904ba
Samantha Nicole Balubal <nicolebalubal@gmail.com>
2016-12-14 06:44:40 -0800

    modified "User_View.php" from views folder

--- exercise 6/CodeIgniter/application/views/User_View.php
+++ exercise 6/CodeIgniter/application/views/User_View.php
@@ -5,82 +5,42 @@
 
 	<style>
 
-	p.intro {
-		font-size: 20px;
-		color: #000000;
-		font-family: Goudy Old Style;
-		text-align: Center; 
-	}
-	
-	p.stylee {
-		font-weight: bold;
-	}
-	
-	footer {
-		font-family: Times New Roman;
-		font-size: 17px;
-		font-color: white;
-	}
-
-	p.hob {
-		font-size: 40px;
-		color: BLACK;
-		font-family: Gigi;
-		text-align: center;
-		font-weight: bold;
-	}
-	
-	p.hob2 {
-		font-size: 25px;
-		font-family: Curlz MT;
-		text-align:center;
-		font-weight: bold;
-	}
-
-	p.con {
-		font-size: 40px;
-		color: BLACK;
-		font-family: Gigi;
-		text-align: center;
-		font-weight: bold;
-	}
-	
-	.error {
-		color: #FF0000;
-		font-size:20px;
-	}
-	img {
-		margin-right: 20px;
-		margin-top: 20px;
-	}
-	body {
-		background-image: url("<?php echo base_url('css/uploads/bg.jpg')?>");
-		background-attachment: fixed;
+	html{
+		background: url(css/pics/pastel_1.jpg);
+		background-repeat: no-repeat center fixed;
+		-webkit-background-size: cover;
+		-moz-background-size: cover;
+		-o-background-size: cover;
+		background-size: cover;
+		
 		table border:0;
-		align:center; 
+		align:center;
 		cellpadding:15;
-		font-family: Garamond;
-		font-size:20px;
+		font-family:Lucida Calligraphy;
+		font-size: 20pt;
 	}
 	
-	.all {
-		align: center;
-		position: relative;
+	
+	p{
+		text-align:center;
+		font-family:Lucida Calligraphy;
+		font-size: 20pt;
+		color: rgb(0, 0, 0); 
+	}
+	
+	#form{
+		font-size: 16pt;
+		color:rgb(0, 0, 0);
+		font-family: Lucida Calligraphy;
 	}
 	
 	#btn {
-		font-family: Garamond;
+		font-family: Lucida Calligraphy;
 		font-size:20px;
 	}
-	
-	.err {
-		font-weight: bold;
-		color: rgb(255,0,0);
-	}
-	
 	table {
 		font-size: 20px;
-		font-family: Goudy Old Style;
+		font-family: Lucida Calligraphy;
 		cellpadding: 5px;
 		table-layout: fixed;
 	}
@@ -142,7 +102,7 @@
 	
 	<tr>
 		<td>
-			<label for="number">Mobile Number:</label>
+			<label for="number">Cellphone Number:</label>
 		</td>
 		<td>
 			<?php echo $form_item['number'];?>
