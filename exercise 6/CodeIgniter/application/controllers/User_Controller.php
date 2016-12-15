45160a7de5429f78273dae195d2f6a8f478aab63
Samantha Nicole Balubal <nicolebalubal@gmail.com>
2016-12-15 15:05:58 -0800

    modified "User_Controller.php"

--- exercise 6/CodeIgniter/application/controllers/User_Controller.php
+++ exercise 6/CodeIgniter/application/controllers/User_Controller.php
@@ -8,82 +8,91 @@
       } 
   
       public function index() { 
-         $query = $this->db->get("users"); 
-         $data['records'] = $query->result(); 
-			
-         $this->load->helper('url'); 
-         $this->load->view('views',$data); 
-      } 
+         $data['Form'] = $this->User_Model->get_users(); 
+         
+         $this->load->view('Form/index.php',$data); 
+      }
+	  
+	  public function User_View($nickname = NULL){
+		  $data['views_item'] = $this->User_Model->get_users($nickname);
+		  
+		  if (empty($data['views_item'])){
+			  show_404();
+		  }
+		  
+		  $data['title'] = $data['views_item'];
+		  
+		  $this->load->view('Form/User_View', $data);
+	  }
+	  
+	  public function User_Create(){
+		  $this->load->helper('Form');
+		  $this->load->library('Form_validation');
+		  
+		  $this->Form_validation->set_rules('name', 'Name', 'required');
+		  $this->Form_validation->set_rules('nickname', 'Nickname', 'required');
+		  $this->Form_validation->set_rules('email', 'Email', 'required');
+		  $this->Form_validation->set_rules('gender', 'Gender', 'required');
+		  $this->Form_validation->set_rules('number', 'Number', 'required');
+		  
+		  if ($this->Form_validation->run() === False)
+		  {
+			  $this->load->view('Form/User_Create');
+		  }
+		  
+		  else
+		  {
+			  $this->User_Model->set_users();
+			  $this->load->view('Form/User_Success');
+		  }
+		  
+	  }
+	  
+	  
+	  public function edit(){
+		  $user_id = $this->uri->segment(3);
+		  
+		  if(empty($user_id)){
+			  show_404();
+		  }
+		  
+		  $this->load->helper('Form');
+		  $this->load->library('Form_validation');
+		  
+		  $data['views_item'] = $this->User_Model->get_user_by_id($user_id);
+		  
+		  $this->Form_validation->set_rules('name', 'Name', 'required');
+		  $this->Form_validation->set_rules('nickname', 'Nickname', 'required');
+		  $this->Form_validation->set_rules('email', 'Email', 'required');
+		  $this->Form_validation->set_rules('gender', 'Gender', 'required');
+		  $this->Form_validation->set_rules('number', 'Number', 'required');
+		  
+		  if ($this->Form_validation->run() === FALSE){
+			  $this->load->view('Form/User_Edit', $data);
+		  }
+		  else {
+			  $this->User_Model->set_users($user_id);
+			  redirect( base_url() . 'index.php/Form');
+		  }
+	  }
   
-      public function add_user_view() { 
-         $this->load->helper('form'); 
-         $this->load->view('user_add'); 
-      } 
   
-      public function add_user() { 
-         $this->load->model('user_model');
-			
-         $data = array( 
-            'user_id' => $this->input->post('user_id'), 
-            'name' => $this->input->post('name'), 
-            'nickname' => $this->input->post('nickname'), 
-            'email' => $this->input->post('email'), 
-            'home' => $this->input->post('home'), 
-            'gender' => $this->input->post('gender'), 
-            'number' => $this->input->post('number'), 
-            'comment' => $this->input->post('comment') 
-
-         ); 
-			
-         $this->user_model->insert($data); 
-   
-         $query = $this->db->get("users"); 
-         $data['records'] = $query->result(); 
-         $this->load->view('user_view',$data); 
+      public function delete() { 
+	  
+		 $user_id = $this->uri->segment('3'); 
+		 
+		 if (empty($user_id)){
+			 show_404();
+		 }
+		 
+		 $views_item = $this->User_Model->get_user_by_id($user_id);
+		 $this->User_Model->delete_user($user_id);
+		 redirect( base_url() . 'index.php/Form');
       } 
-  
-      public function update_user_view() { 
-         $this->load->helper('form'); 
-         $user_id = $this->uri->segment('3'); 
-         $query = $this->db->get_where("users",array("user_id"=>$user_id));
-         $data['records'] = $query->result(); 
-         $data['old_user_id'] = $user_id; 
-         $this->load->view('user_edit',$data); 
-      } 
-  
-      public function update_user(){ 
-         $this->load->model('user_model');
-			
-         $data = array(  
-            'user_id' => $this->input->post('user_id'),
-            'name' => $this->input->post('name'), 
-            'nickname' => $this->input->post('nickname'), 
-            'email' => $this->input->post('email'), 
-            'home' => $this->input->post('home'), 
-            'gender' => $this->input->post('gender'), 
-            'number' => $this->input->post('number'), 
-            'comment' => $this->input->post('comment') 
-
-
-
-         ); 
-			
-         $old_user_id = $this->input->post('old_user_id'); 
-         $this->user_model->update($data,$old_user_id); 
-			
-         $query = $this->db->get("users"); 
-         $data['records'] = $query->result(); 
-         $this->load->view('user_view',$data); 
-      } 
-  
-      public function delete_user() { 
-         $this->load->model('user_model'); 
-         $user_id = $this->uri->segment('3'); 
-         $this->user_model->delete($user_id); 
-   
-         $query = $this->db->get("users"); 
-         $data['records'] = $query->result(); 
-         $this->load->view('user_view',$data); 
-      } 
+	  
+	  public function mypage_view() {
+		  
+		  $this->load->view('Form/mypage_view');
+	  }
    } 
 ?>
