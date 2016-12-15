ad98cbba84ee03ef3f777146595c87ddbac8a7b1
Samantha Nicole Balubal <nicolebalubal@gmail.com>
2016-12-15 15:07:47 -0800

    Edited "User_Model.php"

--- exercise 6/CodeIgniter/application/models/User_Model.php
+++ exercise 6/CodeIgniter/application/models/User_Model.php
@@ -2,25 +2,58 @@
    class User_model extends CI_Model {
 	
       function __construct() { 
-         parent::__construct(); 
+         $this->load->database();
       } 
    
-      public function insert($data) { 
-         if ($this->db->insert("users", $data)) { 
-            return true; 
-         } 
-      } 
+      public function get_users($nickname=False) { 
+	  
+         if ($nickname === false){
+			 $query = $this->db->get('Form');
+			 return $query->result_array();
+		 }
+		 
+		 $query = $this->db->get_where('Form', array('nickname' => $nickname));
+		 return $query->row_array();
+       }
    
-      public function delete($user_id) { 
-         if ($this->db->delete("users", "user_id = ".$user_id)) { 
-            return true; 
-         } 
-      } 
+      public function get_users_by_id($user_id = 0) { 
+	  
+		if ($user_id === 0){
+			$query = $this->db->get('Form');
+			return $query->result_array();
+		}
+		
+		$query = $this->db->get_where('Form',array('user_id' => $user_id));
+		return $query->result_array();
+      }
+	  
+	  public function set_users($user_id = 0)
+	  {
+		  $this->load->helper('url');
+		  
+		  $nickname = url_title($this->input->post('title'),'dash', TRUE);
+		  
+		  $data = array(
+				'name' => $this->input->post('name'),
+				'nickname' => $this->input->post('nickname'),
+				'email' => $this->input->post('email'),
+				'home' => $this->input->post('home'),
+				'gender' => $this->input->post('gender'),
+				'number' => $this->input->post('number'),
+				'comment' => $this->input->post('comment')
+		 );
+		 
+		 if ($user_id == 0) {
+			 return $this->db->insert('Form', $data);
+		 } else {
+			 $this->db->where('user_id', $user_id);
+			 return $this->db->update('Form',$data);
+		 }
+	  }
    
-      public function update($data,$old_user_id) { 
-         $this->db->set($data); 
-         $this->db->where("user_id", $old_user_id); 
-         $this->db->update("users", $data); 
-      } 
-   } 
+      public function delete($user_id) {
+      $this->db->where('user_id', $user_id);
+	  return $this->db->delete('Form');
+   }
+   }
 ?> 
