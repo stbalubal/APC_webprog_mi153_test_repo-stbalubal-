58525bbc63f3b635d6ac93b55df5cad84d29da9f
Samantha Nicole Balubal <nicolebalubal@gmail.com>
2016-12-21 02:40:56 -0800

    Modified db.php

--- exercise 7/yii/basic/config/db.php
+++ exercise 7/yii/basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=contactform',
+    'dsn' => 'mysql:host=localhost;dbname=dbtuts',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
